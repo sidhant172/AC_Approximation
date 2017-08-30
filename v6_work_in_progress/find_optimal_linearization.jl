b8eec538c88c426d022f5d453124f0f3c65eacb6
sidhant172 <31137164+sidhant172@users.noreply.github.com>
2017-08-30 08:28:43 -0600

    experimental v6

---
+++ v6_work_in_progress/find_optimal_linearization.jl
@@ -0,0 +1,227 @@
+using PowerModels
+using JuMP
+using Ipopt
+using Clp
+
+include("opf_mod.jl")
+include("support_functions.jl")
+
+function find_optimal_linearizations(network_data, to_approx_list, inflation_factors, solver, solver_lp, cnst_gen_max_iter, tol, obj_tuning)
+    ######### Defining indices and parameters ######################################
+    ind_bus = [parse(Int,key) for (key,b) in network_data["bus"]]
+    ind_branch = [parse(Int,key) for (key,b) in network_data["branch"]]
+    ind_gen = [parse(Int,key) for (key,b) in network_data["gen"]]
+    ################################################################################
+    slack = [bus["index"] for (i,bus) in network_data["bus"] if bus["bus_type"] == 3][1]
+    ############## Mapping generators to buses #####################################
+    gen_buses = [network_data["gen"][i]["gen_bus"] for i in keys(network_data["gen"])]
+    gen_buses = unique(gen_buses)
+    load_buses = [parse(Int64,i) for i in keys(network_data["bus"]) if abs(network_data["bus"][i]["pd"]) + abs(network_data["bus"][i]["qd"]) > 1e-2]
+    active_buses = union(gen_buses,load_buses)
+
+
+    # remove the next 4 lines
+    # gen_buses = [network_data["gen"][i]["gen_bus"] for i in keys(network_data["gen"])]
+    # gen_buses = unique(gen_buses)
+    # load_buses = [parse(Int64,i) for i in keys(network_data["bus"]) if abs(network_data["bus"][i]["pd"]) + abs(network_data["bus"][i]["qd"]) > 1e-2]
+    # # active_buses = union(gen_buses,load_buses)
+    # active_buses = [b["index"] for (i,b) in network_data["bus"]]
+
+    gens_at_bus = Dict{String,Array{Int,1}}()
+    for i in gen_buses
+        gens_at_bus[string(i)] = [network_data["gen"][j]["index"] for j in keys(network_data["gen"]) if network_data["gen"][j]["gen_bus"] == i]
+    end
+    ################################################################################
+    network_data["ind_gen"] = ind_gen
+    network_data["ind_bus"] = ind_bus
+    network_data["ind_branch"] = ind_branch
+    network_data["gen_buses"] = gen_buses
+    network_data["load_buses"] = load_buses
+    network_data["active_buses"] = active_buses
+    network_data["gen_inflation"] = inflation_factors["gen_inflation"]
+    network_data["load_inflation"] = inflation_factors["load_inflation"]
+    network_data["gens_at_bus"] = gens_at_bus
+    network_data["slack"] = slack
+
+    ############# tighten generator and load limits around OPF solution based on inflation_factors ######################
+    output = run_ac_opf(network_data, solver_ipopt)
+
+    gen_inflation = inflation_factors["gen_inflation"]
+    pg_init = Dict{Int,Float64}()
+    qg_init = Dict{Int,Float64}()
+    baseMVA = output["solution"]["baseMVA"]
+
+    for i in ind_gen
+        pg_init[i] = output["solution"]["gen"][string(i)]["pg"]/baseMVA
+        qg_init[i] = output["solution"]["gen"][string(i)]["qg"]/baseMVA
+        network_data["gen"][string(i)]["pmax"] = max(pg_init[i]*(1+gen_inflation),pg_init[i]*(1-gen_inflation))
+        network_data["gen"][string(i)]["pmin"] = min(pg_init[i]*(1+gen_inflation),pg_init[i]*(1-gen_inflation))
+        network_data["gen"][string(i)]["qmax"] = max(qg_init[i]*(1+gen_inflation),qg_init[i]*(1-gen_inflation))
+        network_data["gen"][string(i)]["qmin"] = min(qg_init[i]*(1+gen_inflation),qg_init[i]*(1-gen_inflation))
+    end
+
+    # network_data["bus"][string(slack)]["vmax"] = (1 + inflation_factors["v_inflation"])*output["solution"]["bus"][string(slack)]["vm"]
+    # network_data["bus"][string(slack)]["vmin"] = (1 - inflation_factors["v_inflation"])*output["solution"]["bus"][string(slack)]["vm"]
+
+    ################################################################################
+
+
+    linear_approximations = Dict{Int64, Any}()  # constainer for all the linear approximations
+    for (i,to_approx) in to_approx_list
+        approximation = find_optimal_linearization(network_data, to_approx, solver, solver_lp, cnst_gen_max_iter, tol, obj_tuning)
+        linear_approximations[i] = approximation
+    end
+    return linear_approximations
+end     # end of find_optimal_linearizations
+
+
+
+
+
+
+
+
+
+
+
+
+
+# find optimal linearization for a given quantity
+function find_optimal_linearization(network_data, to_approx, solver, solver_lp, cnst_gen_max_iter, tol, obj_tuning)
+    approximation = Dict{String,Any}()
+
+    # network_data["to_approx"] = to_approx   # add to network_data
+    network_data["quantity"] = to_approx["quantity"]
+    network_data["quantity_index"] = to_approx["quantity_index"]
+    network_data["obj_tuning"] = obj_tuning
+    active_buses = network_data["active_buses"]
+    gen_buses = network_data["gen_buses"]
+    load_buses = network_data["load_buses"]
+    gens_at_bus = network_data["gens_at_bus"]
+    ind_gen = network_data["ind_gen"]
+    ind_bus = network_data["ind_bus"]
+    ind_branch = network_data["ind_branch"]
+    slack = network_data["slack"]
+
+################################################################################
+    # m = Model(solver=ClpSolver())   # JuMP model for master problem
+    # m = Model(solver=GurobiSolver())
+    m = Model(solver = solver_lp)
+
+    @variable(m, z >=0, start=0)  # maximum error
+    @variable(m,-5<=l_pb[i in active_buses]<=5, start = 0)
+    @variable(m,-5<=l_qb[i in active_buses]<=5, start = 0)
+    @variable(m, -5<=l0<=5, start = 0) # constant term
+    @variable(m, -5<=l_v<=5, start=0)
+
+    @objective(m,Min,z)
+
+    @constraint(m, l_pb[slack] == 0)
+    @constraint(m, l_qb[slack] == 0)
+
+    l0_val = 0
+    l_v_val = 0
+    l_pb_val = Dict{String,Float64}()
+    l_qb_val = Dict{String,Float64}()
+    for i in active_buses
+        l_pb_val[string(i)] = 0.0
+        l_qb_val[string(i)] = 0.0
+    end
+
+
+    converged_flag = 0
+    #lp_err = Float64[];
+    #nlp_err = Float64[];
+    for iter=1:cnst_gen_max_iter   # total iterations of constraint generation
+        status = solve(m)
+        z_val  = getvalue(z)
+        l0_val = getvalue(l0)
+        l_v_val = getvalue(l_v)
+        for i in active_buses
+            l_pb_val[string(i)] = getvalue(l_pb[i])
+            l_qb_val[string(i)] = getvalue(l_qb[i])
+        end
+
+        # add current linear coefficients to network data
+        network_data["l0"] = l0_val
+        network_data["l_v"] = l_v_val
+        network_data["l_pb"] = l_pb_val
+        network_data["l_qb"] = l_qb_val
+
+        #push!(lp_err,abs(z_val))
+        #push!(nlp_err,0)
+
+        converged_flag = 1
+
+    ################################################################################
+        network_data["direction"] = 0   # direction of maximization
+        (result, pm_model) = run_ac_opf_mod(network_data,solver)
+
+        @show lp_error = z_val - result["objective"]/obj_tuning
+        #nlp_err[iter] = abs(z_val - s["objective"])
+
+        if lp_error < -tol
+            converged_flag = 0
+
+            current_sol = get_current_solution(result["solution"], pm_model, to_approx, ind_gen, ind_bus, ind_branch)
+            @show current_sol["val"]
+
+            approximation["worst_case_lower"] = current_sol
+
+            @constraint(m, current_sol["val"] - (l0 + l_v*current_sol["vm"][slack] +
+                sum(l_pb[i]*( sum(current_sol["pg"][j] for j in gens_at_bus[string(i)]) ) + l_qb[i]*( sum(current_sol["qg"][j] for j in gens_at_bus[string(i)]) ) for i in gen_buses)
+            -   sum(l_pb[i]*current_sol["pd"][i]  + l_qb[i]*current_sol["qd"][i] for i in load_buses) )
+                <= z)
+
+            # add neighborhood bundle cuts
+            if abs(lp_error) < 100*tol
+                add_bundle_cuts(m,current_sol,network_data,radius,num)
+            end
+        end
+    ################################################################################
+
+    ################################################################################
+        network_data["direction"] = 1   # direction of maximization
+        (result, pm_model) = run_ac_opf_mod(network_data,solver)
+
+        @show lp_error = z_val - result["objective"]/obj_tuning
+        #nlp_err[iter] = abs(z_val - s["objective"])
+
+        if lp_error < -tol
+            converged_flag = 0
+
+            current_sol = get_current_solution(result["solution"], pm_model, to_approx, ind_gen, ind_bus, ind_branch)
+            @show current_sol["val"]
+
+            approximation["worst_case_upper"] = current_sol
+
+            @constraint(m, -current_sol["val"] + (l0 + l_v*current_sol["vm"][slack] +
+            sum(l_pb[i]*( sum(current_sol["pg"][j] for j in gens_at_bus[string(i)]) ) + l_qb[i]*( sum(current_sol["qg"][j] for j in gens_at_bus[string(i)]) ) for i in gen_buses)
+        -   sum(l_pb[i]*current_sol["pd"][i]  + l_qb[i]*current_sol["qd"][i] for i in load_buses) )
+            <= z)
+
+            # add neighborhood bundle cuts
+            if abs(lp_error) < 100*tol
+                add_bundle_cuts(m,current_sol,network_data,radius,num)
+            end
+        end
+    ################################################################################
+
+    @show iter
+    @show z_val
+
+        if converged_flag == 1
+            println("Constraint generation converged!")
+            break
+        end
+    end     # end of constraint generation loop
+    ################################################################################
+
+    approximation["l0"] = l0_val
+    approximation["l_v"] = l_v_val
+    approximation["l_pb"] = l_pb_val
+    approximation["l_qb"] = l_qb_val
+    approximation["error"] = getvalue(z)
+
+    return approximation
+end     # end of find optimal linearizaion
---
+++ v6_work_in_progress/opf_mod.jl
@@ -0,0 +1,168 @@
+using PowerModels
+using JuMP
+
+
+PM = PowerModels
+
+function run_ac_opf_mod(data, solver)
+    pm = build_generic_model(data, ACPPowerModel, post_opf_mod)
+    solution = solve_generic_model(pm, solver; solution_builder = PowerModels.get_solution)
+    return solution, pm.model
+end
+
+
+function post_opf_mod(pm::PM.GenericPowerModel)
+    # standard variable definitions using PowerModels inbuild functions
+    PM.variable_voltage(pm)
+    PM.variable_generation(pm)
+    PM.variable_line_flow(pm)
+    PM.variable_dcline_flow(pm)
+
+    variable_load(pm)   # custom function to add load as variables
+
+    add_objective(pm)   # custom function that adds objective
+
+    add_slack_constraint(pm)
+
+    # add_power_factor_constraint(pm)     # custom function that adds constant power factor constraint
+
+
+    # Rest of the constraints using PowerModels in-built functions
+    PM.constraint_voltage(pm)
+    for (i,bus) in pm.ref[:ref_buses]
+        PM.constraint_theta_ref(pm, bus)
+    end
+
+    for (i,bus) in pm.ref[:bus]
+        #PM.constraint_kcl_shunt(pm, bus)
+        constraint_kcl_shunt_mod(pm, bus)
+    end
+
+    for (i,branch) in pm.ref[:branch]
+        PM.constraint_ohms_yt_from(pm, branch)
+        PM.constraint_ohms_yt_to(pm, branch)
+
+        PM.constraint_phase_angle_difference(pm, branch)
+
+        PM.constraint_thermal_limit_from(pm, branch)
+        PM.constraint_thermal_limit_to(pm, branch)
+    end
+    for (i,dcline) in pm.ref[:dcline]
+        PM.constraint_dcline(pm, dcline)
+    end
+
+end     # end post_opf_mod
+
+
+
+
+# custom kcl constraint to accommodate loads as variables
+function constraint_kcl_shunt_mod{T <: PM.AbstractACPForm}(pm::PM.GenericPowerModel{T}, bus)
+    i = bus["index"]
+    bus_arcs = pm.ref[:bus_arcs][i]
+    bus_arcs_dc = pm.ref[:bus_arcs_dc][i]
+    bus_gens = pm.ref[:bus_gens][i]
+
+    pd = bus["pd"]
+    qd = bus["qd"]
+    gs = bus["gs"]
+    bs = bus["bs"]
+
+    v = getindex(pm.model, :v)[i]
+    p = getindex(pm.model, :p)
+    q = getindex(pm.model, :q)
+    pg = getindex(pm.model, :pg)
+    qg = getindex(pm.model, :qg)
+    p_dc = getindex(pm.model, :p_dc)
+    q_dc = getindex(pm.model, :q_dc)
+
+
+    pd_var = getindex(pm.model, :pd)[i]
+    qd_var = getindex(pm.model, :qd)[i]
+    #q_dc = getindex(pm.model, :q_dc)
+
+    c1 = @constraint(pm.model, sum(p[a] for a in bus_arcs) + sum(p_dc[a_dc] for a_dc in bus_arcs_dc) == sum(pg[g] for g in bus_gens) - pd_var - gs*v^2)
+    c2 = @constraint(pm.model, sum(q[a] for a in bus_arcs) + sum(q_dc[a_dc] for a_dc in bus_arcs_dc) == sum(qg[g] for g in bus_gens) - qd_var + bs*v^2)
+    return Set([c1, c2])
+end     # end constraint_kcl_shunt_mod
+
+
+# custom unction to add objective
+function add_objective(pm::PM.GenericPowerModel)
+    direction = pm.data["direction"]
+
+    quantity = pm.data["quantity"]
+    quantity_index = pm.data["quantity_index"]
+
+    gen_buses = pm.data["gen_buses"]
+    load_buses = pm.data["load_buses"]
+    active_buses = pm.data["active_buses"]
+    # gens_at_bus = pm.data["gens_at_bus"]
+    # bus_gens = pm.ref[:bus_gens][i]
+
+    sign = (-1)^direction
+
+    # access JuMP variables
+    v = getindex(pm.model, :v)  # voltage magnitude variable
+    t = getindex(pm.model, :t)  # voltage angle variable
+    p = getindex(pm.model, :p)  # line active flow variables
+    q = getindex(pm.model, :q)  # line reactive flow variables
+    pg = getindex(pm.model, :pg)   # active generation variables
+    qg = getindex(pm.model, :qg)   # reactive generation variables
+    pd = getindex(pm.model, :pd)
+    qd = getindex(pm.model, :qd)
+
+    # get current linearization coefficients
+    l0_val = pm.data["l0"]
+    l_v_val = pm.data["l_v"]
+    l_pb_val = pm.data["l_pb"]
+    l_qb_val = pm.data["l_qb"]
+    obj_tuning = pm.data["obj_tuning"]
+
+    slack = pm.data["slack"]
+
+
+    if quantity == "line_real_power"
+        @objective(pm.model, Max, obj_tuning*(sign*p[quantity_index] - sign*(l0_val + l_v_val*v[slack] +
+            sum(l_pb_val[string(i)]*(sum(pg[j] for j in pm.ref[:bus_gens][i]))  +  l_qb_val[string(i)]*(sum(qg[j] for j in pm.ref[:bus_gens][i]))   for i in gen_buses)
+            -  sum(l_pb_val[string(i)]*pd[i] + l_qb_val[string(i)]*qd[i]  for i in load_buses)  ))
+            )
+        elseif quantity == "line_reactive_power"
+            @objective(pm.model, Max, obj_tuning*(sign*q[quantity_index] - sign*(l0_val + l_v_val*v[slack] +
+            sum(l_pb_val[string(i)]*(sum(pg[j] for j in pm.ref[:bus_gens][i]))  +  l_qb_val[string(i)]*(sum(qg[j] for j in pm.ref[:bus_gens][i]))   for i in gen_buses)
+            -  sum(l_pb_val[string(i)]*pd[i] + l_qb_val[string(i)]*qd[i]  for i in load_buses)  ))
+            )
+        elseif quantity == "bus_voltage_magnitude"
+            @objective(pm.model, Max, obj_tuning*(sign*v[quantity_index] - sign*(l0_val + l_v_val*v[slack] +
+            sum(l_pb_val[string(i)]*(sum(pg[j] for j in pm.ref[:bus_gens][i]))  +  l_qb_val[string(i)]*(sum(qg[j] for j in pm.ref[:bus_gens][i]))   for i in gen_buses)
+            -  sum(l_pb_val[string(i)]*pd[i] + l_qb_val[string(i)]*qd[i]  for i in load_buses)  ))
+            )
+        else println("is not supported.")
+    end
+end
+
+# custom function to add power factor constraint
+function add_power_factor_constraint(pm::PM.GenericPowerModel)
+    pd = getindex(pm.model, :pd)
+    qd = getindex(pm.model, :qd)
+    @constraint(pm.model, power_factor[i in keys(pm.ref[:bus])], qd[i]*pm.ref[:bus][i]["pd"] == pd[i]*pm.ref[:bus][i]["qd"])
+    # @constraint(pm.model, power_factor_low[i in keys(pm.ref[:bus])], 0.95*pd[i]*pm.ref[:bus][i]["qd"] <= qd[i]*pm.ref[:bus][i]["pd"])
+    # @constraint(pm.model, power_factor_high[i in keys(pm.ref[:bus])], qd[i]*pm.ref[:bus][i]["pd"] <= 1.05*pd[i]*pm.ref[:bus][i]["qd"])
+end
+
+# custom function to add load as variables
+function variable_load(pm::GenericPowerModel)
+    # Defining new variables for load real and reactive power
+    load_inflation = pm.data["load_inflation"]
+	# @variable(pm.model, (1-load_inflation)*pm.ref[:bus][i]["pd"] <= pd[i in keys(pm.ref[:bus])] <= (1+load_inflation)*pm.ref[:bus][i]["pd"])
+    # @variable(pm.model, (1-load_inflation)*pm.ref[:bus][i]["qd"] <= qd[i in keys(pm.ref[:bus])] <= (1+load_inflation)*pm.ref[:bus][i]["qd"])
+    @variable(pm.model, min((1-load_inflation)*pm.ref[:bus][i]["pd"],(1+load_inflation)*pm.ref[:bus][i]["pd"]) <=
+        pd[i in keys(pm.ref[:bus])] <= max((1-load_inflation)*pm.ref[:bus][i]["pd"],(1+load_inflation)*pm.ref[:bus][i]["pd"]))
+    @variable(pm.model, min((1-load_inflation)*pm.ref[:bus][i]["qd"],(1+load_inflation)*pm.ref[:bus][i]["qd"]) <=
+        qd[i in keys(pm.ref[:bus])] <= max((1-load_inflation)*pm.ref[:bus][i]["qd"],(1+load_inflation)*pm.ref[:bus][i]["qd"]))
+end
+
+function add_slack_constraint(pm::GenericPowerModel)
+    t = getindex(pm.model, :t)
+    @constraint(pm.model, t[pm.data["slack"]] == 0)
+end
