31219ea7b338c64015f2af080919744baf14fccb
sidhant172 <31137164+sidhant172@users.noreply.github.com>
2017-09-14 09:20:58 -0600

    minor

--- v7/Optimized_linear.jl
+++ v7/Optimized_linear.jl
@@ -2,8 +2,8 @@ using PowerModels
 using JuMP
 using Ipopt
 # using Clp
-using GLPKMathProgInterface
-# using Gurobi
+# using GLPKMathProgInterface
+using Gurobi
 
 include("opf_mod.jl")
 include("support_functions.jl")
@@ -46,9 +46,9 @@ solver_ipopt = IpoptSolver(print_level=0) # , linear_solver="ma97"
 # solver_ipopt = IpoptSolver(print_level=0, linear_solver="ma97")
 # solver_ipopt = IpoptSolver(linear_solver="ma97")
 
-solver_lp = GLPKSolverLP()
+# solver_lp = GLPKSolverLP()
 # solver_lp = ClpSolver()
-# solver_lp = GurobiSolver()
+solver_lp = GurobiSolver()
 
 
 
