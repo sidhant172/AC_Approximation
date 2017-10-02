0a7552d1867f1639664512ed15e69d28bb48f04f
sidhant172 <31137164+sidhant172@users.noreply.github.com>
2017-10-02 17:52:27 -0600

    updating

--- v7/Plots/make_plots.jl
+++ v7/Plots/make_plots.jl
@@ -7,8 +7,9 @@ using PowerModels
 
 # filename = "nesta_case57_ieee.m"
 # filename = "nesta_case30_as.m"
-filename = "nesta_case14_ieee.m"
+# filename = "nesta_case14_ieee.m"
 
+filename = ARGS[1]
 
 network_data = PowerModels.parse_file("../"string(filename))
 num_branch = length(network_data["branch"])
