c45dfc8407da557e4ce8d64d9f787b01ec63401a
sidhant172 <31137164+sidhant172@users.noreply.github.com>
2017-10-05 19:33:06 -0600

    updating

--- v7/Plots/make_heatmaps.jl
+++ v7/Plots/make_heatmaps.jl
@@ -37,6 +37,11 @@ qq_jac = vars_jac["Hac_f"][39:76,25:48]
 p_error = zeros(num_branch,length(inflation_factors))
 q_error = zeros(num_branch,length(inflation_factors))
 
+pp_dict = Dict{String,Any}()
+pq_dict = Dict{String,Any}()
+qp_dict = Dict{String,Any}()
+qq_dict = Dict{String,Any}()
+
 
 ctr = 0
 for inflation in inflation_factors
@@ -51,6 +56,12 @@ for inflation in inflation_factors
     qq = vars["coeff_q"]
     q_err = vars["approx_error"]
     q_error[:,ctr] = q_err
+
+    pp_dict[string(convert(Int64,inflation*100))] = pp
+    pq_dict[string(convert(Int64,inflation*100))] = pq
+    qp_dict[string(convert(Int64,inflation*100))] = qp
+    qq_dict[string(convert(Int64,inflation*100))] = qq
+
     # println("showing results for radius = ",inflation)
     # @show norm(pp[:,active_buses]-pp_jac[:,active_buses])
     # @show norm(pq[:,active_buses]-pq_jac[:,active_buses])
@@ -71,11 +82,30 @@ for inflation in inflation_factors
     # savefig(heatmap(1:num_active,1:38,abs(qp[:,active_buses]-qp_jac[:,active_buses]),aspect_ratio=1, xaxis=(0:4:20)),"plots"string(num_bus)"/heatmaps/heatmap_qp_"string(convert(Int64,inflation*100))".pdf")
     # savefig(heatmap(1:num_active,1:38,abs(qq[:,active_buses]-qq_jac[:,active_buses]),aspect_ratio=1, xaxis=(0:4:20)),"plots"string(num_bus)"/heatmaps/heatmap_qq_"string(convert(Int64,inflation*100))".pdf")
 
-    savefig(heatmap(1:38,1:num_active,abs(pp[:,active_buses]-pp_jac[:,active_buses])',aspect_ratio=1, yaxis=(0:2:20)),"plots"string(num_bus)"/heatmaps/heatmap_pp_"string(convert(Int64,inflation*100))".pdf")
-    savefig(heatmap(1:38,1:num_active,abs(pq[:,active_buses]-pq_jac[:,active_buses])',aspect_ratio=1, yaxis=(0:2:20)),"plots"string(num_bus)"/heatmaps/heatmap_pq_"string(convert(Int64,inflation*100))".pdf")
-    savefig(heatmap(1:38,1:num_active,abs(qp[:,active_buses]-qp_jac[:,active_buses])',aspect_ratio=1, yaxis=(0:2:20)),"plots"string(num_bus)"/heatmaps/heatmap_qp_"string(convert(Int64,inflation*100))".pdf")
-    savefig(heatmap(1:38,1:num_active,abs(qq[:,active_buses]-qq_jac[:,active_buses])',aspect_ratio=1, yaxis=(0:2:20)),"plots"string(num_bus)"/heatmaps/heatmap_qq_"string(convert(Int64,inflation*100))".pdf")
+    # savefig(heatmap(1:38,1:num_active,abs(pp[:,active_buses]-pp_jac[:,active_buses])',aspect_ratio=1, yaxis=(0:2:20)),"plots"string(num_bus)"/heatmaps/heatmap_pp_"string(convert(Int64,inflation*100))".pdf")
+    # savefig(heatmap(1:38,1:num_active,abs(pq[:,active_buses]-pq_jac[:,active_buses])',aspect_ratio=1, yaxis=(0:2:20)),"plots"string(num_bus)"/heatmaps/heatmap_pq_"string(convert(Int64,inflation*100))".pdf")
+    # savefig(heatmap(1:38,1:num_active,abs(qp[:,active_buses]-qp_jac[:,active_buses])',aspect_ratio=1, yaxis=(0:2:20)),"plots"string(num_bus)"/heatmaps/heatmap_qp_"string(convert(Int64,inflation*100))".pdf")
+    # savefig(heatmap(1:38,1:num_active,abs(qq[:,active_buses]-qq_jac[:,active_buses])',aspect_ratio=1, yaxis=(0:2:20)),"plots"string(num_bus)"/heatmaps/heatmap_qq_"string(convert(Int64,inflation*100))".pdf")
 end
+
+
+
+combined_heatmap = heatmap(size = (1400,800), layout=4, right_margin=10px, left_margin=10px, top_margin= 0px, bottom_margin=0px,
+    1:38,1:num_active, abs(pp_dict["5"][:,active_buses]-pp_jac[:,active_buses])'
+    ,aspect_ratio=1, yaxis=(0:2:20))
+heatmap!(1:38,1:num_active, abs(pq_dict["5"][:,active_buses]-pp_jac[:,active_buses])', aspect_ratio=1, yaxis=(0:2:20), subplot=2)
+heatmap!(1:38,1:num_active, abs(pp_dict["40"][:,active_buses]-pp_jac[:,active_buses])', aspect_ratio=1, yaxis=(0:2:20), subplot=3)
+heatmap!(1:38,1:num_active, abs(pq_dict["40"][:,active_buses]-pp_jac[:,active_buses])', aspect_ratio=1, yaxis=(0:2:20), subplot=4)
+
+annotate!(2,18,text("Lp R=0.05",:white,:left), subplot=1)
+annotate!(2,18,text("Lq R=0.05",:white,:left), subplot=2)
+annotate!(2,18,text("Lp R=0.4",:white,:left), subplot=3)
+annotate!(2,18,text("Lq R=0.4",:white,:left), subplot=4)
+
+savefig(combined_heatmap,"combined_heatmap.pdf")
+
+# combined_heatmap = heatmap(size = (1400,900), layout=4, right_margin=20px, left_margin=20px, top_margin= 20px, bottom_margin=10px,
+# 1:num_active,1:38,abs(pp_dict["5"][:,active_buses]-pp_jac[:,active_buses]),aspect_ratio=1, "combined_heatmap.pdf" )
 
 # for i = 1:num_branch
 #     savefig(plot(inflation_factors,p_error[i,:],xlabel="Radius",ylabel="Maximum approximation error",leg=false),"plots"string(num_bus)"/approximation_error/approx_error_real_line_"string(i)".pdf")
---
+++ v7/Plots/plot_error_tracking.jl
@@ -0,0 +1,55 @@
+using MAT
+using Plots
+gr()
+
+vars = matread("../results24/error_tracking_real_line1.mat")
+# vars = matread("../results57/error_tracking_real_line1.mat")
+nlp_err = max(vars["nlp_err_pos"],vars["nlp_err_neg"])
+lp_err = vars["lp_err"]
+
+
+
+err_plot = plot(size = (1400,800), layout=4, right_margin=40px, left_margin=20px, top_margin= 30px, bottom_margin=10px, layout=4,
+    nlp_err[2:end], xlabel = "Iteration number", ylabel="Maximum approximation error", leg=false,  xtickfont = font(10, "Courier"), linewidth=2)
+    
+plot!(lp_err, xlabel = "Iteration number", ylabel="Objective of LP relaxation", color=:red, xtickfont = font(10, "Courier"),
+     linewidth=2, leg=false, subplot=3, right_margin=40px, left_margin=20px, top_margin= 30px, bottom_margin=10px,)
+# savefig(err_real,"plots24/error_tracking_real.pdf")
+
+lp_deltas_real = vars["lp_deltas"]
+
+
+vars = matread("../results24/error_tracking_reactive_line1.mat")
+# vars = matread("../results57/error_tracking_reactive_line1.mat")
+
+nlp_err = max(vars["nlp_err_pos"],vars["nlp_err_neg"])
+lp_err = vars["lp_err"]
+plot!(nlp_err[2:end], xlabel = "Iteration number", ylabel="Maximum approximation error",xtickfont = font(10, "Courier"), linewidth=2, leg=false, subplot=2, right_margin=40px, left_margin=20px, top_margin= 30px, bottom_margin=10px)
+# plot!(nlp_err)
+plot!(lp_err, xlabel = "Iteration number", ylabel="Objective of LP relaxation", xtickfont = font(10, "Courier"),color=:red, linewidth=2, leg=false, subplot=4, right_margin=40px, left_margin=20px, top_margin= 30px, bottom_margin=10px)
+
+lp_deltas_reactive = vars["lp_deltas"]
+
+annotate!(65,1,text("Line #1 Active power", font(9), :left), subplot=1)
+annotate!(95,0.8,text("Line #1 Reactive power", font(9), :left), subplot=2)
+annotate!(65,0.0004,text("Line #1 Active power", font(9), :left), subplot=3)
+annotate!(95,0.0016,text("Line #1 Reactive power", font(9), :left), subplot=4)
+
+# savefig(plot(vars["lp_deltas"]), "plots24/lp_deltas_reactive.pdf")
+
+
+# plot!(lp_deltas_real, xlabel = "Iteration number", ylabel="Change in lp",xtickfont = font(10, "Courier"), linewidth=2, color=:green, leg=false, subplot=3, right_margin=40px, left_margin=20px, top_margin= 30px, bottom_margin=10px)
+# plot!(nlp_err)
+# plot!(lp_deltas_reactive, xlabel = "Iteration number", ylabel="Change in lq", xtickfont = font(10, "Courier"),color=:green, linewidth=2, leg=false, subplot=4, right_margin=40px, left_margin=20px, top_margin= 30px, bottom_margin=10px)
+
+
+
+savefig(err_plot,"plots24/error_tracking.pdf")
+# savefig(err_plot,"plots57/error_tracking.pdf")
+
+
+
+
+
+# savefig(plot(lp_deltas_real),"plots24/lp_deltas_real.pdf")
+# savefig(plot(lp_deltas_reactive),"plots24/lp_deltas_reactive.pdf")
