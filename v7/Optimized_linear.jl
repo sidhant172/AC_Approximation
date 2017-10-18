29ec38051d5d33679ee12481e0090c085088d4ff
sidhant172 <31137164+sidhant172@users.noreply.github.com>
2017-10-18 09:50:23 -0600

    update

--- v7/Optimized_linear.jl
+++ v7/Optimized_linear.jl
@@ -67,14 +67,16 @@ inflation_factors["load_inflation"] = load_inflation
 to_approx_list = Dict{Int64,Any}()
 
 
+line_num = 18
+
 to_approx = Dict{String,Any}()
 to_approx["quantity"] = "line_real_power"
-to_approx["quantity_index"] = (1,network_data["branch"]["1"]["f_bus"],network_data["branch"]["1"]["t_bus"])
+to_approx["quantity_index"] = (1,network_data["branch"][string(line_num)]["f_bus"],network_data["branch"]["1"]["t_bus"])
 to_approx_list[1] = to_approx
 
 to_approx = Dict{String,Any}()
 to_approx["quantity"] = "line_reactive_power"
-to_approx["quantity_index"] = (1,network_data["branch"]["1"]["f_bus"],network_data["branch"]["1"]["t_bus"])
+to_approx["quantity_index"] = (1,network_data["branch"][string(line_num)]["f_bus"],network_data["branch"]["1"]["t_bus"])
 to_approx_list[2] = to_approx
 
 # to_approx = Dict{String,Any}()
