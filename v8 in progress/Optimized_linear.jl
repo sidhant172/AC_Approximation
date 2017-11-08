16912de268255e305fc3ea155fc84d9b0f99c1e0
sidhant172 <31137164+sidhant172@users.noreply.github.com>
2017-11-08 11:54:38 -0700

    update

--- v8 in progress/Optimized_linear.jl
+++ v8 in progress/Optimized_linear.jl
@@ -43,7 +43,7 @@ load_inflation = 0.3    # defining range of generation conditions
 
 tol = gen_inflation*1e-2
 
-obj_tuning = 1e2
+obj_tuning = 1e1
 
 # quantity = "line_real_power"
 # quantity_to_approx = "line_reactive_power"
