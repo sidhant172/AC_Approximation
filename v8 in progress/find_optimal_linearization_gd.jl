d5ca10aa95654b15d0e89ec01b639b10d63d1f39
sidhant172 <31137164+sidhant172@users.noreply.github.com>
2017-11-08 11:34:49 -0700

    update

--- v8 in progress/find_optimal_linearization_gd.jl
+++ v8 in progress/find_optimal_linearization_gd.jl
@@ -157,7 +157,7 @@ for iter = 1:cnst_gen_max_iter
     # (result, pm) = run_ac_opf_mod(network_data,solver)
     pm_0 = build_generic_model(network_data, ACPPowerModel, post_opf_mod)
     set_warm_start(pm_0,pm_0_old)
-    result = solve_generic_model(pm_0, IpoptSolver(print_level=0,mu_init=1e-8); solution_builder = PowerModels.get_solution)
+    result = solve_generic_model(pm_0, IpoptSolver(print_level=0,mu_init=1e-5); solution_builder = PowerModels.get_solution)
     current_sol = get_current_solution(result["solution"], pm_0, to_approx, ind_gen, ind_bus, ind_branch)
     val0 = result["objective"]/obj_tuning
 
@@ -173,7 +173,7 @@ for iter = 1:cnst_gen_max_iter
     network_data["direction"] = 1
     pm_1 = build_generic_model(network_data, ACPPowerModel, post_opf_mod)
     set_warm_start(pm_1,pm_1_old)
-    result = solve_generic_model(pm_1, IpoptSolver(print_level=0,mu_init=1e-8); solution_builder = PowerModels.get_solution)
+    result = solve_generic_model(pm_1, IpoptSolver(print_level=0,mu_init=1e-5); solution_builder = PowerModels.get_solution)
     current_sol = get_current_solution(result["solution"], pm_1, to_approx, ind_gen, ind_bus, ind_branch)
     val1 = result["objective"]/obj_tuning
 
