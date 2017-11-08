bedb6b9a326c6c04e818b94bb5363358cf5c4882
sidhant172 <31137164+sidhant172@users.noreply.github.com>
2017-11-08 15:27:22 -0700

    u

--- v8 in progress/find_optimal_linearization_gd.jl
+++ v8 in progress/find_optimal_linearization_gd.jl
@@ -162,6 +162,7 @@ step_factor = 1
 
 warm = true
 backtrack = true
+ctr = 0
 
 for iter = 1:cnst_gen_max_iter
 
@@ -229,7 +230,8 @@ for iter = 1:cnst_gen_max_iter
 
 
     if @show 0.5*(val0 + val1) > err + 0*tol  &&  backtrack == true
-        step_factor = step_factor*2
+        # step_factor = step_factor*2
+        ctr = ctr + 1
         for i in active_buses
             l_pb_val[string(i)] = l_pb_val_old[string(i)]
             l_qb_val[string(i)] = l_qb_val_old[string(i)]
@@ -237,7 +239,7 @@ for iter = 1:cnst_gen_max_iter
         warm = false
         backtrack =  false
 
-        if step_factor > 2^10
+        if ctr = 5
             break
         end
         continue # skip doing gradient descent step
