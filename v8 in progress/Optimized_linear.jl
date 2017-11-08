501da8e228997bcc0ffa8a9cf28431861c2e082f
sidhant172 <31137164+sidhant172@users.noreply.github.com>
2017-11-08 16:29:57 -0700

    u

--- v8 in progress/Optimized_linear.jl
+++ v8 in progress/Optimized_linear.jl
@@ -37,8 +37,8 @@ end
 # tol = 1e-4   # convergence tolerance
 
 # operational conditions
-gen_inflation = 0.4   # defining range of loading conditions
-load_inflation = 0.4    # defining range of generation conditions
+gen_inflation = 0.25  # defining range of loading conditions
+load_inflation = 0.25    # defining range of generation conditions
 # v_inflation = 0.1
 
 tol = gen_inflation*1e-2
