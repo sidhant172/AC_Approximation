ed6cab6bf3d67aa3bb3b596b5eeffbd573f8e5e9
sidhant172 <31137164+sidhant172@users.noreply.github.com>
2017-11-08 16:38:18 -0700

    u

--- v8 in progress/Optimized_linear.jl
+++ v8 in progress/Optimized_linear.jl
@@ -37,8 +37,8 @@ end
 # tol = 1e-4   # convergence tolerance
 
 # operational conditions
-gen_inflation = 0.3  # defining range of loading conditions
-load_inflation = 0.3    # defining range of generation conditions
+gen_inflation = 0.1  # defining range of loading conditions
+load_inflation = 0.1    # defining range of generation conditions
 # v_inflation = 0.1
 
 tol = gen_inflation*1e-2
@@ -49,10 +49,10 @@ obj_tuning = 1e1
 # quantity_to_approx = "line_reactive_power"
 # quantity_to_approx = "bus_voltage_magnitude"
 
-network_data = PowerModels.parse_file("case24_ieee_rts.m")
+# network_data = PowerModels.parse_file("case24_ieee_rts.m")
 # network_data = PowerModels.parse_file("nesta_case14_ieee.m")
 # network_data = PowerModels.parse_file("nesta_case30_as.m")
-# network_data = PowerModels.parse_file("case118.m")
+network_data = PowerModels.parse_file("case118.m")
 # network_data = PowerModels.parse_file("nesta_case57_ieee.m")
 # network_data = PowerModels.parse_file("nesta_case300_ieee.m")
 
