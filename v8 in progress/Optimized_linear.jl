27225979a4e78cdd732c226cba6420753680b654
sidhant172 <31137164+sidhant172@users.noreply.github.com>
2017-11-15 13:25:54 -0700

    darwin

--- v8 in progress/Optimized_linear.jl
+++ v8 in progress/Optimized_linear.jl
@@ -7,7 +7,7 @@ using GLPKMathProgInterface
 using MAT
 
 
-algo = 2
+algo = 1
 num_samples = 100
 
 include("opf_mod.jl")
@@ -35,9 +35,9 @@ include("find_linearization_error.jl")
 if algo == 0
     cnst_gen_max_iter = 1000
 elseif algo == 1
-    cnst_gen_max_iter = 15
+    cnst_gen_max_iter = 20
 elseif algo == 2
-    cnst_gen_max_iter = 5
+    cnst_gen_max_iter = 6
 end
 
 # algorithm parameters
@@ -45,8 +45,8 @@ end
 # tol = 1e-4   # convergence tolerance
 
 # operational conditions
-gen_inflation = 0.2 # defining range of loading conditions
-load_inflation = 0.2    # defining range of generation conditions
+gen_inflation = 0.3 # defining range of loading conditions
+load_inflation = 0.3    # defining range of generation conditions
 # v_inflation = 0.1
 
 tol = gen_inflation*1e-3
