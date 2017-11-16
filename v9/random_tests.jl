78dd88a402d11d38040fa61e90ee4af6915731a9
sidhant172 <31137164+sidhant172@users.noreply.github.com>
2017-11-16 09:51:50 -0700

    darwin

--- v9/random_tests.jl
+++ v9/random_tests.jl
@@ -1,62 +1,42 @@
 using JuMP
 using Ipopt
-using PowerModels
+using Plots
+gr()
 
 
-
-
-######################### JuMP user defined functions ########################
-function f(c,x...)
-    return sum((x[i]-c)^2 for i in 1:length(x))
-end
-
-function fprime(g,c,x...)
-    for i=1:length(x)
-        g[i] = 2*(x[i]-c)
-    end
-end
-
-function obj(x...)
-    
-end
-
-# function fdprime(h,x...)
-#     for i=1:length
+# thetavals =  0:0.1:10
+# corr = zeros(length(thetavals))
+#
+#
+# for i=1:length(thetavals)
+#
+#     theta = thetavals[i]
+#
+#     m = Model(solver=IpoptSolver())
+#
+#     @variable(m,0<=s<=1)
+#
+#     @NLobjective(m,Max,-s*log(s)-(1-s)*log(1-s) + 0.5*theta*(1-2s)^2)
+#
+#     status = solve(m)
+#
+#     @show sval = getvalue(s)
+#
+#     @show corr[i] = (exp(2*theta*(1-2*sval))-1)/(exp(2*theta*(1-2*sval))+1)
 # end
 
-m = Model(solver=IpoptSolver(hessian_approximation="limited-memory"))
 
-JuMP.register(m,:f,2,f,fprime)
-# JuMP.register(m,:f,4,f,autodiff=true)
+c = 0.1
 
-@variable(m,x[1:2])
-# @variable(m,y)
-# @variable(m,z)
 
-# args = (x[1],x[2])
-# @NLobjective(m, Min, f(x[1],x[2]))
-JuMP.setNLobjective(m, :Min, Expr(:call, :f, c,x...))
+# for i=10:1000
+i = 10000
+    A = (1-c/i)*eye(i) + (c/i)*ones(i,i)
+    @show minimum(eigvals(A))
+# end
 
-# JuMP.setNLobjective(m, :Min, Expr(:call, :+,
-#                                         Expr(:call, :obj, [x[i] for i=1:num_spins]...),
-#                                         Expr(:call, :l1norm, [z[i] for i=1:num_spins]...)
-#                                             Expr(:call, :obj, x...),
-#                                             Expr(:call, :l1norm, z...)
-#                                         )
-#                             )
 
-# @NLconstraint(m, f(x...) <= z)
 
-status = solve(m)
-########################################################################
-
-# x = [3,2]
-#
-#
-# @show f(x...)
-
-# function f(x...)
-#     return sum(x[i] for i in 1:length(x))
 # end
 #
 # x = [1,2,3,4]
