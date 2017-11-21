8832d22d24f3acdae99dd8e09074bba279fde2c4
sidhant172 <31137164+sidhant172@users.noreply.github.com>
2017-11-21 14:59:20 -0700

    darwin

--- v9/random_tests_new.jl
+++ v9/random_tests_new.jl
@@ -12,7 +12,7 @@ varmin = eye(n)
 
 for t23 = -gamma:0.02:gamma
     for t12 = -(gamma-abs(t23)):0.02:(gamma-abs(t23)), t13 = -(gamma-abs(t23)):0.02:(gamma-abs(t23))
-        if (abs(t13)+(t12) > gamma)
+        if (abs(t13)+abs(t12) > gamma)
             continue
         end
         theta = eye(n)
